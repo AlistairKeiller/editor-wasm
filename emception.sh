21fcb3ed8ebd351cb2a9cebf7e28cd88a01428c6
Alistair Keiller <alistair@keiller.net>
2022-07-09 15:01:48 -0700

    copy first bit from build.sh

--- emception.sh
+++ emception.sh
@@ -2,4 +2,16 @@ apt-get install -y python3 cmake ninja-build
 
 git clone https://github.com/jprendes/emception
 cd emception
-bash build-llvm.sh
+SRC=$(dirname $0)
+BUILD="$1"
+
+if [ "$BUILD" == "" ]; then
+    BUILD=$(pwd)/build
+fi
+
+SRC=$(realpath "$SRC")
+BUILD=$(realpath "$BUILD")
+
+$SRC/build-tooling.sh "$BUILD"
+
+$SRC/build-llvm.sh "$BUILD" "$LLVM_SRC"
