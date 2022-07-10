06d0248c1e3744af34d6c7570e16bcc8cd7dbf22
Alistair Keiller <alistair@keiller.net>
2022-07-09 21:09:21 -0700

    remove -it

--- emception.sh
+++ emception.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-git clone https://github.com/jprendes/emception
-cd emception
-bash build-with-docker.sh
+SRC=$(dirname $0)
+SRC=$(realpath "$SRC")
+
+pushd $SRC/docker
+docker build \
+    -t emception_build \
+    .
+popd
+
+mkdir -p $(pwd)/build/emsdk_cache
+
+docker run \
+    --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $(pwd):$(pwd) \
+    -v $(pwd)/build/emsdk_cache:/emsdk/upstream/emscripten/cache \
+    -u $(id -u):$(id -g) \
+    $(id -G | tr ' ' '\n' | xargs -I{} echo --group-add {}) \
+    emception_build:latest \
+    bash -c "cd $(pwd) && ./build.sh"
