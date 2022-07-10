a8ca35f2dea012a4f0a0b09403d589d5537b1634
Alistair Keiller <alistair@keiller.net>
2022-07-09 20:25:35 -0700

    fix emception

--- emception.sh
+++ emception.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 set -e
 
-# nessesary packages
-apt-get install -y python3 cmake ninja-build
-
-# install emscription
-git clone https://github.com/emscripten-core/emsdk
-cd emsdk
-./emsdk install latest
-./emsdk activate latest
-source ./emsdk_env.sh
-cd ..
-
 git clone https://github.com/jprendes/emception
 cd emception
-SRC=$(dirname $0)
-BUILD="$1"
-
-if [ "$BUILD" == "" ]; then
-    BUILD=$(pwd)/build
-fi
-
-SRC=$(realpath "$SRC")
-BUILD=$(realpath "$BUILD")
-
-$SRC/build-tooling.sh "$BUILD"
-
-$SRC/build-llvm.sh "$BUILD" "$LLVM_SRC"
+./build_with_docker.sh
