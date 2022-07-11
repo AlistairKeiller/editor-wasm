4ad7f2a1109c58b32ebb32253fb688c9e1988624
Alistair Keiller <alistair@keiller.net>
2022-07-11 16:39:30 -0700

    test

--- docker.sh
+++ docker.sh
@@ -4,9 +4,12 @@ docker build \
     -t emception_build \
     .
 
-docker run --rm \
+docker run \
+    --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(pwd):$(pwd) \
     -v $(pwd)/build/emsdk_cache:/emsdk/upstream/emscripten/cache \
+    -u $(id -u):$(id -g) \
+    $(id -G | tr ' ' '\n' | xargs -I{} echo --group-add {}) \
     emception_build:latest \
-    bash -c "cd $(pwd) && ./build.sh"
+    bash -c "cd $(pwd) && ./build-llvm.sh"
