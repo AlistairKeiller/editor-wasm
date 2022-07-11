d9b816bbffd3e73fd0ce868803a99b34a156819d
Alistair Keiller <alistair@keiller.net>
2022-07-11 16:37:01 -0700

    test

--- docker.sh
+++ docker.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+docker build \
+    -t emception_build \
+    .
+
 docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(pwd):$(pwd) \
     -v $(pwd)/build/emsdk_cache:/emsdk/upstream/emscripten/cache \
-    emscripten/emsdk:latest \
+    emception_build:latest \
     bash -c "cd $(pwd) && ./build.sh"
