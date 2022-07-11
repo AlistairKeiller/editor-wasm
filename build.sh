f0a3131daa42ec1b3fd1927f4723c266af362c9d
Alistair Keiller <alistair@keiller.net>
2022-07-10 21:12:27 -0700

    debug

--- build.sh
+++ build.sh
@@ -24,11 +24,22 @@ cmake --build local-build -- clang-tblgen llvm-tblgen
 
 
 # build wasm llvm
-emcmake cmake -G Ninja -S llvm -B web-build \
+# emcmake cmake -G Ninja -S llvm -B web-build \
+#         -DCMAKE_BUILD_TYPE=Release \
+#         -DCMAKE_CROSSCOMPILING=True \
+#         -DCMAKE_INSTALL_PREFIX=install \
+#         -DCMAKE_CXX_FLAGS='-O3 -s NO_ASSERTIONS -s NO_INVOKE_RUN -s EXIT_RUNTIME -s ALLOW_MEMORY_GROWTH -s INITIAL_MEMORY=64MB -s MODULARIZE -s EXPORT_ES6 -s EXTRA_EXPORTED_RUNTIME_METHODS=["callMain","FS"]' \
+#         -DLLVM_ENABLE_THREADS=OFF \
+#         -DLLVM_DEFAULT_TARGET_TRIPLE=wasm32-wasi \
+#         -DLLVM_TARGET_ARCH=wasm32-emscripten \
+#         -DLLVM_ENABLE_PROJECTS=lld \
+#         -DLLVM_TABLEGEN=$PWD/local-build/bin/llvm-tblgen
+
+EMCC_DEBUG=2 emcmake cmake -G Ninja -S llvm -B web-build \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_CROSSCOMPILING=True \
         -DCMAKE_INSTALL_PREFIX=install \
-        -DCMAKE_CXX_FLAGS='-O3 -s NO_ASSERTIONS -s NO_INVOKE_RUN -s EXIT_RUNTIME -s ALLOW_MEMORY_GROWTH -s INITIAL_MEMORY=64MB -s MODULARIZE -s EXPORT_ES6 -s EXTRA_EXPORTED_RUNTIME_METHODS=["callMain","FS"]' \
+        -DCMAKE_CXX_FLAGS='-g2 -s ASSERTIONS=2 -s SAFE_HEAP -s STACK_OVERFLOW_CHECK=2 -s DEMANGLE_SUPPORT -s MALLOC=emmalloc-debug -s EXCEPTION_DEBUG -s PTHREADS_DEBUG -s ABORT_ON_WASM_EXCEPTIONS -s NO_INVOKE_RUN -s EXIT_RUNTIME -s ALLOW_MEMORY_GROWTH -s INITIAL_MEMORY=64MB -s MODULARIZE -s EXPORT_ES6 -s EXTRA_EXPORTED_RUNTIME_METHODS=["callMain","FS"]' \
         -DLLVM_ENABLE_THREADS=OFF \
         -DLLVM_DEFAULT_TARGET_TRIPLE=wasm32-wasi \
         -DLLVM_TARGET_ARCH=wasm32-emscripten \
