ce5ce3e294d05cf96829516378029ce34cc965fd
Alistair Keiller <alistair@keiller.net>
2022-07-11 16:29:29 -0700

    test

--- docker.sh
+++ docker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(pwd):$(pwd) \
+    -v $(pwd)/build/emsdk_cache:/emsdk/upstream/emscripten/cache \
+    -u $(id -u):$(id -g) \
+    $(id -G | tr ' ' '\n' | xargs -I{} echo --group-add {}) \
     emscripten/emsdk:latest \
-    bash -c "cd $(pwd) && build.sh"
+    bash -c "cd $(pwd) && ./build.sh"
