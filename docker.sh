3cd04bc38355e0c1df7c6a37b615cf684b0f1e2a
Alistair Keiller <alistair@keiller.net>
2022-07-11 16:32:15 -0700

    remove

--- docker.sh
+++ docker.sh
@@ -4,6 +4,5 @@ docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(pwd):$(pwd) \
     -v $(pwd)/build/emsdk_cache:/emsdk/upstream/emscripten/cache \
-    $(id -G | tr ' ' '\n' | xargs -I{} echo --group-add {}) \
     emscripten/emsdk:latest \
     bash -c "cd $(pwd) && ./build.sh"
