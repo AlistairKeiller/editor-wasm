8b5aa27d4faf802ffcae79faddff80131100cdbf
Alistair Keiller <alistair@keiller.net>
2022-07-11 16:31:56 -0700

    set user

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(pwd):$(pwd) \
     -v $(pwd)/build/emsdk_cache:/emsdk/upstream/emscripten/cache \
-    -u $(id -u):$(id -g) \
+    -u root \
     $(id -G | tr ' ' '\n' | xargs -I{} echo --group-add {}) \
     emscripten/emsdk:latest \
     bash -c "cd $(pwd) && ./build.sh"
