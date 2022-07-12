3e7d91af8f73f499ac7cddd3c53ecece1fa3e80b
Alistair Keiller <alistair@keiller.net>
2022-07-11 17:58:02 -0700

    test

--- docker.sh
+++ docker.sh
@@ -4,18 +4,8 @@ SRC=$(dirname $0)
 SRC=$(realpath "$SRC")
 chmod +x $SRC/build.sh
 
-docker build \
-    -t emception_build \
-    .
-
-mkdir -p $(pwd)/build/emsdk_cache
-
 docker run \
     --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(pwd):$(pwd) \
-    -v $(pwd)/build/emsdk_cache:/emsdk/upstream/emscripten/cache \
-    -u $(id -u):$(id -g) \
-    $(id -G | tr ' ' '\n' | xargs -I{} echo --group-add {}) \
-    emception_build:latest \
+    emscripten/emsdk:latest \
     bash -c "cd $(pwd) && ./build.sh"
