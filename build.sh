cbe1d48ffe51431b8273caff5168bf1f1fcdc8c4
Alistair Keiller <alistair@keiller.net>
2022-07-12 16:50:58 -0700

    add compiler flags and remove cmake flags

--- build.sh
+++ build.sh
@@ -14,18 +14,19 @@ cmake --build local-build -- clang-tblgen llvm-tblgen
 
 
 # build wasm llvm
-# LDFLAGS="-s LLD_REPORT_UNDEFINED=1 -s ALLOW_MEMORY_GROWTH=1 -s EXPORTED_FUNCTIONS=_main,_free,_malloc -s EXPORTED_RUNTIME_METHODS=FS,PROXYFS,allocateUTF8 -lproxyfs.js" \
+LDFLAGS="-s LLD_REPORT_UNDEFINED=1 -s ALLOW_MEMORY_GROWTH=1 -s EXPORTED_FUNCTIONS=_main,_free,_malloc -s EXPORTED_RUNTIME_METHODS=FS,PROXYFS,allocateUTF8 -lproxyfs.js" \
 CXXFLAGS="-Dwait4=__syscall_wait4" \
 emcmake cmake -G Ninja -S llvm -B web-build \
         -DCMAKE_BUILD_TYPE=Release \
-        -DLLVM_TARGETS_TO_BUILD=WebAssembly \
         -DLLVM_ENABLE_PROJECTS="clang;lld;clang-tools-extra" \
+        -DLLVM_ENABLE_THREADS=OFF \
         -DLLVM_TABLEGEN=$PWD/local-build/bin/llvm-tblgen \
         -DCLANG_TABLEGEN=$PWD/local-build/bin/clang-tblgen \
-        -DCMAKE_CROSSCOMPILING=True \
-        -DCMAKE_INSTALL_PREFIX=install \
-        -DLLVM_DEFAULT_TARGET_TRIPLE=wasm32-wasi \
-        -DLLVM_TARGET_ARCH=wasm32-emscripten
+        # -DLLVM_TARGETS_TO_BUILD=WebAssembly \
+        # -DCMAKE_CROSSCOMPILING=True \
+        # -DCMAKE_INSTALL_PREFIX=install \
+        # -DLLVM_DEFAULT_TARGET_TRIPLE=wasm32-wasi \
+        # -DLLVM_TARGET_ARCH=wasm32-emscripten
 
 
 
