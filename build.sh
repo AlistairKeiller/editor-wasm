8aa1f662ecb5167f8e60a3c69f903a8e3f60b6de
Alistair Keiller <alistair@keiller.net>
2022-07-14 20:42:14 -0700

    add clang-tools-extra

--- build.sh
+++ build.sh
@@ -15,15 +15,16 @@ ninja -C local-build -- clang-tblgen llvm-tblgen
 # LDFLAGS="-s LLD_REPORT_UNDEFINED=1 -s ALLOW_MEMORY_GROWTH=1 -s EXPORTED_FUNCTIONS=_main,_free,_malloc -s EXPORTED_RUNTIME_METHODS=FS,PROXYFS,allocateUTF8 -lproxyfs.js" \
 CXXFLAGS="-Dwait4=__syscall_wait4" \
 emcmake cmake -G Ninja -S llvm -B web-build \
-        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_BUILD_TYPE=MinSizeRel \
         -DLLVM_TARGETS_TO_BUILD=WebAssembly \
         -DLLVM_ENABLE_THREADS=OFF \
         -DLLVM_INCLUDE_TESTS=OFF \
+        -DLLVM_BUILD_TOOLS=OFF \
         -DLLVM_TABLEGEN=$PWD/local-build/bin/llvm-tblgen \
         -DCLANG_TABLEGEN=$PWD/local-build/bin/clang-tblgen \
-        -DLLVM_ENABLE_PROJECTS="clang;lld" \
-        # -DCMAKE_CROSSCOMPILING=True \
-        # -DCMAKE_INSTALL_PREFIX=install \
-        # -DLLVM_DEFAULT_TARGET_TRIPLE=wasm32-wasi \
-        # -DLLVM_TARGET_ARCH=wasm32-emscripten
-ninja -C web-build -j1
+        -DLLVM_ENABLE_PROJECTS="clang;lld;clang-tools-extra" \
+        -DLLVM_PARALLEL_LINK_JOBS=1 \
+        # LLVM_BUILD_LLVM_DYLIB=ON \
+        # LLVM_LINK_LLVM_DYLIB=ON \
+
+ninja -C web-build
