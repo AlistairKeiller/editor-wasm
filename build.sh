b29e478d3af9cfa9e7fba5dba8781cce311a9b2c
Alistair Keiller <alistair@keiller.net>
2022-07-25 16:38:08 -0700

    modularize

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ ninja -C local-build -- clang-tblgen llvm-tblgen
 
 # build wasm llvm
 CXXFLAGS="-Dwait4=__syscall_wait4" \
-LDFLAGS="-s EXPORT_ES6" \
+LDFLAGS="-s EXPORT_ES6 -s MODULARIZE" \
 emcmake cmake -G Ninja -S llvm -B web-build \
         -DCMAKE_BUILD_TYPE=MinSizeRel \
         -DLLVM_TARGETS_TO_BUILD=WebAssembly \
