fa72f976a4065bda8734db763378f707bd3ae42e
Alistair Keiller <alistair@keiller.net>
2022-07-25 16:35:25 -0700

    move flag

--- build.sh
+++ build.sh
@@ -13,12 +13,12 @@ ninja -C local-build -- clang-tblgen llvm-tblgen
 
 # build wasm llvm
 CXXFLAGS="-Dwait4=__syscall_wait4" \
+LDFLAGS="-s EXPORT_ES6" \
 emcmake cmake -G Ninja -S llvm -B web-build \
         -DCMAKE_BUILD_TYPE=MinSizeRel \
         -DLLVM_TARGETS_TO_BUILD=WebAssembly \
         -DLLVM_INCLUDE_TESTS=OFF \
         -DLLVM_BUILD_TOOLS=OFF \
-        -DCMAKE_CXX_FLAGS='-s EXPORT_ES6' \
         -DLLVM_TABLEGEN=$PWD/local-build/bin/llvm-tblgen \
         -DCLANG_TABLEGEN=$PWD/local-build/bin/clang-tblgen \
         -DLLVM_ENABLE_PROJECTS="clang;lld;clang-tools-extra" \
