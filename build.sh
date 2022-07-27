ccf6a326a5dfb8ee053b059be73c9937b42f08e5
Alistair Keiller <alistair@keiller.net>
2022-07-26 17:05:50 -0700

    add exported functions and memory growth

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ ninja -C local-build -- clang-tblgen llvm-tblgen
 
 
 # build wasm llvm
-CXXFLAGS="-Dwait4=__syscall_wait4" \
-LDFLAGS="-s EXPORT_ES6 -s MODULARIZE" \
+CXXFLAGS="-D wait4=__syscall_wait4" \
+LDFLAGS="-s EXPORTED_FUNCTIONS=_main -s EXPORTED_RUNTIME_METHODS=FS s ALLOW_MEMORY_GROWTH=1 -s EXPORT_ES6 -s MODULARIZE" \
 emcmake cmake -G Ninja -S llvm -B web-build \
         -DCMAKE_BUILD_TYPE=MinSizeRel \
         -DLLVM_TARGETS_TO_BUILD=WebAssembly \
