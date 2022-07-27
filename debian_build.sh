f7dd778e652f7d9547ea70b921491b2e9e2437ad
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2022-07-26 22:59:41 -0700

    Update debian_build.sh

--- debian_build.sh
+++ debian_build.sh
@@ -2,9 +2,7 @@
 set -e
 
 # install nessesary pacakges
-apt-get install -y clang ninja-build libc++-dev
-apt-get update
-apt-get upgrade -y
+apt-get install -y clang ninja-build
 
 # install emscripten
 source emscripten.sh
