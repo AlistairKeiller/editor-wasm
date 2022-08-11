c38fcb9e025d90db16da195f5cbe7b36fe92f3b1
Alistair Keiller <alistair@keiller.net>
2022-08-11 07:14:27 -0700

    remove LDFLAGS

--- build.sh
+++ build.sh
@@ -16,8 +16,7 @@ mv wasi-sysroot wsysroot
 # sed -i '/# Add appropriate flags for GCC/c\set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --preload-file=wsysroot")' clang/CMakeLists.txt
 
 # build wasm llvm
-CXXFLAGS="-Dwait4=__syscall_wait4" \
-LDFLAGS='-sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE' \
+CXXFLAGS="-Dwait4=__syscall_wait4 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE" \
 emcmake cmake -G Ninja -S llvm -B web-build \
         -DCMAKE_BUILD_TYPE=MinSizeRel \
         -DLLVM_TARGETS_TO_BUILD=WebAssembly \
