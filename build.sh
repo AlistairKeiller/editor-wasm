32c2d14770b932eeed902fadbe962ae57d73f7aa
Alistair Keiller <alistair@keiller.net>
2022-08-11 06:49:45 -0700

    test preload

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ ninja -C local-build -- clang-tblgen llvm-tblgen
 wget -qO- https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-16/wasi-sysroot-16.0.tar.gz | tar -xz
 mv wasi-sysroot wsysroot
 
-sed -i '/# Add appropriate flags for GCC/c\set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --embed-file=wsysroot")' clang/CMakeLists.txt
+sed -i '/# Add appropriate flags for GCC/c\set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --preload-file=wsysroot")' clang/CMakeLists.txt
 
 # build wasm llvm
 emcmake cmake -G Ninja -S llvm -B web-build \
