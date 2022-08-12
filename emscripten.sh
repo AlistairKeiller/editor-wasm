29475baae980deda9732fb1767884fe8c2b3b5f4
Alistair Keiller <alistair@keiller.net>
2022-08-12 11:56:19 -0700

    set emscripten version

--- emscripten.sh
+++ emscripten.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://github.com/emscripten-core/emsdk
 cd emsdk
-./emsdk install latest
-./emsdk activate latest
+./emsdk install 3.1.15
+./emsdk activate 3.1.15
 source ./emsdk_env.sh
 cd ..
