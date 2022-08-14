accc1cfddb2a6ec4611ac1939e968350aa068daa
Alistair Keiller <alistair@keiller.net>
2022-08-13 22:39:23 -0700

    better version number

--- debian_build.sh
+++ debian_build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # install nessesary pacakges
-apt-get install -y ninja-build cmake=3.16.3
+apt-get install -y ninja-build cmake=3.16.3-1ubuntu1
 
 # install emscripten
 source emscripten.sh
