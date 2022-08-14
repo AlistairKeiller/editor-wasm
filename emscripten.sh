67c4a7ec1f1567c0b2ae393fa9cd80de12908402
Alistair Keiller <alistair@keiller.net>
2022-08-13 21:54:40 -0700

    test latest

--- emscripten.sh
+++ emscripten.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git clone https://github.com/emscripten-core/emsdk
 cd emsdk
-./emsdk install 3.1.6
-./emsdk activate 3.1.6
+./emsdk install latest
+./emsdk activate latest
 source ./emsdk_env.sh
 cd ..
