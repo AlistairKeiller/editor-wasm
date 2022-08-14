6fd66b3e122eb44971fc7df04064ce9c1a1b8036
Alistair Keiller <alistair@keiller.net>
2022-08-13 22:11:44 -0700

    change cd

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # git clone https://github.com/llvm/llvm-project
 # cd llvm-project
 wget -qO- https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-14.0.6.tar.gz | tar -xz
-cd llvmorg-14.0.6
+cd llvm-project-llvmorg-14.0.6
 cmake -G Ninja -S llvm -B local-build \
         -DCMAKE_BUILD_TYPE=Release \
         -DLLVM_ENABLE_PROJECTS=clang
