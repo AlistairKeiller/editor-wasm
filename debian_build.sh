dc16e6ff8eb393fb04059c47cb6219cd3585e62c
Alistair Keiller <alistair@keiller.net>
2022-08-13 22:58:00 -0700

    set cmake to apt

--- debian_build.sh
+++ debian_build.sh
@@ -2,7 +2,9 @@
 set -e
 
 # install nessesary pacakges
-apt-get install -y ninja-build
+apt-get install -y ninja-build cmake
+rm /bin/cmake
+ln -s /bin/cmake /usr/bin/cmake
 
 # install emscripten
 source emscripten.sh
