6e681b8b3f7dc532e5fec2227cadfa5ee0c15c0c
Alistair Keiller <alistair@keiller.net>
2022-08-14 10:50:42 -0700

    change export

--- build.sh
+++ build.sh
@@ -12,21 +12,20 @@ git clone https://github.com/WebAssembly/binaryen
 cmake -G Ninja -S binaryen -B binaryen-build \
         -DBUILD_TESTS=OFF
 ninja -C binaryen-build
-export PATH=$PWD/binaryen-build:$PATH
+export PATH=$PWD/binaryen-build/bin:$PATH
 
 # download llvm
 git clone https://github.com/llvm/llvm-project
-cd llvm-project
 
 # build llvm for host
-cmake -G Ninja -S llvm -B local-build \
+cmake -G Ninja -S llvm-project/llvm -B host-llvm-build \
         -DCMAKE_BUILD_TYPE=Release \
         -DLLVM_ENABLE_PROJECTS="lld;clang" \
         -DLLVM_TARGETS_TO_BUILD="host;WebAssembly" \
         -DLLVM_INCLUDE_EXAMPLES=OFF \
         -DLLVM_INCLUDE_TESTS=OFF
-ninja -C local-build
-export PATH=$PWD/local-build:$PATH
+ninja -C host-llvm-build
+export PATH=$PWD/host-llvm-build/bin:$PATH
 
 # test emscripten
 emcc --generate-config
@@ -43,13 +42,13 @@ emcc --check
 # build wasm llvm
 CXXFLAGS="-Dwait4=__syscall_wait4" \
 LDFLAGS='-sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE' \
-emcmake cmake -G Ninja -S llvm -B web-build \
+emcmake cmake -G Ninja -S llvm-project/llvm -B web-build \
         -DCMAKE_BUILD_TYPE=MinSizeRel \
         -DLLVM_TARGETS_TO_BUILD=WebAssembly \
         -DLLVM_INCLUDE_TESTS=OFF \
         -DLLVM_BUILD_TOOLS=OFF \
-        -DLLVM_TABLEGEN=$PWD/local-build/bin/llvm-tblgen \
-        -DCLANG_TABLEGEN=$PWD/local-build/bin/clang-tblgen \
+        -DLLVM_TABLEGEN=$PWD/host-llvm-build/bin/llvm-tblgen \
+        -DCLANG_TABLEGEN=$PWD/host-llvm-build/bin/clang-tblgen \
         -DLLVM_ENABLE_PROJECTS="clang" \
         -DLLVM_PARALLEL_LINK_JOBS=1
 
