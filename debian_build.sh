93fef06b28d52d81490e3e41f4b4f49270022e46
Alistair Keiller <alistair@keiller.net>
2022-08-13 22:59:55 -0700

    dont change smlink

--- debian_build.sh
+++ debian_build.sh
@@ -3,9 +3,9 @@ set -e
 
 # install nessesary pacakges
 apt-get install -y ninja-build cmake
-ls -l /bin/cmake
+# ls -l /bin/cmake
 rm /bin/cmake
-ln -s /bin/cmake /usr/bin/cmake
+# ln -s /bin/cmake /usr/bin/cmake
 
 # install emscripten
 source emscripten.sh
