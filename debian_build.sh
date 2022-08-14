bc0057e429a67eb31b05d1d3ecb0259024efa52e
Alistair Keiller <alistair@keiller.net>
2022-08-13 22:46:33 -0700

    pruge cmake

--- debian_build.sh
+++ debian_build.sh
@@ -2,7 +2,8 @@
 set -e
 
 # install nessesary pacakges
-apt-get install -y ninja-build cmake=3.16.3-1ubuntu1
+apt-get purge cmake
+apt-get install -y ninja-build # cmake=3.16.3-1ubuntu1
 
 # install emscripten
 source emscripten.sh
