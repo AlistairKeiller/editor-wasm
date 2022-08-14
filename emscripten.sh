43d4da89dddba3e89ed3e910e80d6bf8708b0644
Alistair Keiller <alistair@keiller.net>
2022-08-13 21:50:59 -0700

    change emscripten version

--- emscripten.sh
+++ emscripten.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git clone https://github.com/emscripten-core/emsdk
 cd emsdk
-./emsdk install 3.1.17
-./emsdk activate 3.1.17
+./emsdk install 3.1.6
+./emsdk activate 3.1.6
 source ./emsdk_env.sh
 cd ..
