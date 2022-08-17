3939261d98670dfd3c84f3f5282f9f5db604fdbb
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2022-08-16 22:38:34 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,8 +15,7 @@ cmake -G Ninja -S llvm-project/llvm -B host-llvm-build \
         -DLLVM_INCLUDE_BENCHMARKS=OFF \
         -DLLVM_INCLUDE_EXAMPLES=OFF \
         -DLLVM_INCLUDE_TESTS=OFF \
-        -DLLVM_CCACHE_BUILD=ON \
-        -DLLVM_CCACHE_DIR=~/.cache/ccache
+        -DLLVM_CCACHE_BUILD=ON
 ninja -C host-llvm-build
 export PATH=$PWD/host-llvm-build/bin:$PATH
 
@@ -57,6 +56,5 @@ emcmake cmake -G Ninja -S llvm-project/llvm -B web-llvm-build \
         -DLLVM_INCLUDE_EXAMPLES=OFF \
         -DLLVM_INCLUDE_TESTS=OFF \
         -DCMAKE_CXX_FLAGS='-Dwait4=__syscall_wait4 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE' \
-        -DLLVM_CCACHE_BUILD=ON \
-        -DLLVM_CCACHE_DIR=~/.cache/ccache
+        -DLLVM_CCACHE_BUILD=ON
 ninja -C web-llvm-build -- clang
