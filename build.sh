255fa4409a7a442756303dc16f5789733105e212
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2022-08-17 08:35:04 -0700

    test ccache

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 
 set -e
 
+mkdir -p ~/.cache/ccache
+
 # build llvm for host
 git clone https://github.com/llvm/llvm-project
 cmake -G Ninja -S llvm-project/llvm -B host-llvm-build \
@@ -15,46 +17,48 @@ cmake -G Ninja -S llvm-project/llvm -B host-llvm-build \
         -DLLVM_INCLUDE_BENCHMARKS=OFF \
         -DLLVM_INCLUDE_EXAMPLES=OFF \
         -DLLVM_INCLUDE_TESTS=OFF \
-        -DLLVM_CCACHE_BUILD=ON
+        -DLLVM_CCACHE_BUILD=ON \
+        -DLLVM_CCACHE_DIR=~/.cache/ccache
 ninja -C host-llvm-build
-export PATH=$PWD/host-llvm-build/bin:$PATH
+# export PATH=$PWD/host-llvm-build/bin:$PATH
 
 # build binaryen for host
-git clone https://github.com/WebAssembly/binaryen
-cmake -G Ninja -S binaryen -B binaryen-build \
-        -DCMAKE_C_COMPILER=clang \
-        -DCMAKE_CXX_COMPILER=clang++ \
-        -DBUILD_TESTS=OFF
-ninja -C binaryen-build
-export PATH=$PWD/binaryen-build/bin:$PATH
+# git clone https://github.com/WebAssembly/binaryen
+# cmake -G Ninja -S binaryen -B binaryen-build \
+#         -DCMAKE_C_COMPILER=clang \
+#         -DCMAKE_CXX_COMPILER=clang++ \
+#         -DBUILD_TESTS=OFF
+# ninja -C binaryen-build
+# export PATH=$PWD/binaryen-build/bin:$PATH
 
 # install emscripten
-git clone https://github.com/AlistairKeiller/emscripten
-cd emscripten
-npm i
-export PATH=$PWD:$PATH
-cd ..
-emcc --generate-config
+# git clone https://github.com/AlistairKeiller/emscripten
+# cd emscripten
+# npm i
+# export PATH=$PWD:$PATH
+# cd ..
+# emcc --generate-config
 
 # build sysroot
-git clone https://github.com/WebAssembly/wasi-libc
-cd wasi-libc
-make
-cd ..
+# git clone https://github.com/WebAssembly/wasi-libc
+# cd wasi-libc
+# make
+# cd ..
 
-sed -i '877i set_target_properties(clang PROPERTIES COMPILE_FLAGS --preload-file=.)' llvm-project/clang/CMakeLists.txt
+# sed -i '877i set_target_properties(clang PROPERTIES COMPILE_FLAGS --preload-file=.)' llvm-project/clang/CMakeLists.txt
 
 # build wasm llvm
-emcmake cmake -G Ninja -S llvm-project/llvm -B web-llvm-build \
-        -DCMAKE_BUILD_TYPE=MinSizeRel \
-        -DLLVM_ENABLE_PROJECTS="clang" \
-        -DLLVM_TARGETS_TO_BUILD=WebAssembly \
-        -DLLVM_TABLEGEN=$PWD/host-llvm-build/bin/llvm-tblgen \
-        -DCLANG_TABLEGEN=$PWD/host-llvm-build/bin/clang-tblgen \
-        -DLLVM_PARALLEL_LINK_JOBS=1 \
-        -DLLVM_INCLUDE_BENCHMARKS=OFF \
-        -DLLVM_INCLUDE_EXAMPLES=OFF \
-        -DLLVM_INCLUDE_TESTS=OFF \
-        -DCMAKE_CXX_FLAGS='-Dwait4=__syscall_wait4 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE' \
-        -DLLVM_CCACHE_BUILD=ON
-ninja -C web-llvm-build -- clang
+# emcmake cmake -G Ninja -S llvm-project/llvm -B web-llvm-build \
+#         -DCMAKE_BUILD_TYPE=MinSizeRel \
+#         -DLLVM_ENABLE_PROJECTS="clang" \
+#         -DLLVM_TARGETS_TO_BUILD=WebAssembly \
+#         -DLLVM_TABLEGEN=$PWD/host-llvm-build/bin/llvm-tblgen \
+#         -DCLANG_TABLEGEN=$PWD/host-llvm-build/bin/clang-tblgen \
+#         -DLLVM_PARALLEL_LINK_JOBS=1 \
+#         -DLLVM_INCLUDE_BENCHMARKS=OFF \
+#         -DLLVM_INCLUDE_EXAMPLES=OFF \
+#         -DLLVM_INCLUDE_TESTS=OFF \
+#         -DCMAKE_CXX_FLAGS='-Dwait4=__syscall_wait4 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE' \
+#         -DLLVM_CCACHE_BUILD=ON \
+#         -DLLVM_CCACHE_DIR=~/.cache/ccache
+# ninja -C web-llvm-build -- clang
