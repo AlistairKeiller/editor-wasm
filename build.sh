6b7aaeace5168a332146eff6418330f375de2e8c
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2022-08-18 09:31:52 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -40,7 +40,7 @@ cd wasi-libc
 make
 cd ..
 
-sed -i '498i set_target_properties(clang PROPERTIES COMPILE_FLAGS --preload-file=.)' llvm-project/clang/CMakeLists.txt
+sed -i '502i set_target_properties(clang PROPERTIES COMPILE_FLAGS --preload-file=.)' llvm-project/clang/CMakeLists.txt
 
 # build wasm llvm
 emcmake cmake -G Ninja -S llvm-project/llvm -B web-llvm-build \
