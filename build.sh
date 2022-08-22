b432122fb8a12b583c5eb9f616d648100bb988e7
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2022-08-22 10:55:18 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -53,6 +53,6 @@ emcmake cmake -G Ninja -S llvm-project/llvm -B web-llvm-build \
         -DLLVM_INCLUDE_TESTS=OFF \
         -DLLVM_CCACHE_BUILD=ON \
         -DLLVM_CCACHE_DIR=/tmp/ccache \
-        -DCMAKE_CXX_FLAGS='-Dwait4=__syscall_wait4 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE'
+#         -DCMAKE_CXX_FLAGS='-Dwait4=__syscall_wait4 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE'
 # mv wasi-libc/sysroot web-llvm-build
 ninja -C web-llvm-build -- clang
