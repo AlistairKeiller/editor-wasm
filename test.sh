8d583dd75c9692acf11a5cf17a7de6af28c654e3
Alistair Keiller <alistair@keiller.net>
2022-08-23 09:45:15 -0700

    test

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+git clone https://github.com/emscripten-core/emsdk.git
+cd emsdk
+./emsdk install latest
+./emsdk activate latest
+./emsdk_env.sh
+
+git clone https://github.com/llvm/llvm-project
+emcmake cmake -G Ninja -S llvm-project/llvm -B web-llvm-build \
+        -DCMAKE_BUILD_TYPE=MinSizeRel \
+        -DLLVM_ENABLE_PROJECTS="clang" \
+        -DLLVM_TARGETS_TO_BUILD=WebAssembly \
+        -DLLVM_PARALLEL_LINK_JOBS=1 \
+        -DLLVM_INCLUDE_BENCHMARKS=OFF \
+        -DLLVM_INCLUDE_EXAMPLES=OFF \
+        -DLLVM_INCLUDE_TESTS=OFF \
+        -DLLVM_CCACHE_BUILD=ON \
+        -DLLVM_CCACHE_DIR=/tmp/ccache \
+        -DCMAKE_CXX_FLAGS='-Dwait4=__syscall_wait4 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE'
+ninja -C web-llvm-build -- clang
