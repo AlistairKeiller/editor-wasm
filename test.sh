90f81f7d7cb750848409af4da051dc4138ab3348
Alistair Keiller <alistair@keiller.net>
2022-08-23 17:42:28 -0700

    test

--- test.sh
+++ test.sh
@@ -37,7 +37,7 @@ emcmake ./cmake-3.23.3-linux-x86_64/bin/cmake -G Ninja -S llvm-project/llvm -B w
         -DLLVM_CCACHE_BUILD=ON \
         -DLLVM_CCACHE_DIR=/tmp/ccache \
         -DDEFAULT_SYSROOT=/sysroot \
-        -DCMAKE_CXX_FLAGS='-Dwait4=__syscall_wait4 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE'
+        -DCMAKE_CXX_FLAGS='-Dwait4=__syscall_wait4 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE --max-memory=4294967296'
 # mv wasi-libc/sysroot web-llvm-build
 mv wasi-sysroot web-llvm-build/sysroot
 ninja -C web-llvm-build -- clang
