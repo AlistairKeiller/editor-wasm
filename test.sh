6cb507a30294e7a7b5f03b40c59cd9d7618fc220
Alistair Keiller <alistair@keiller.net>
2022-08-23 19:50:40 -0700

    test

--- test.sh
+++ test.sh
@@ -14,18 +14,17 @@ cd emsdk
 cd ..
 
 # build sysroot
-# git clone https://github.com/WebAssembly/wasi-libc
-# cd wasi-libc
-# make
-# cd ..
-wget -qO- https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-16/wasi-sysroot-16.0.tar.gz | tar -xz
+git clone https://github.com/WebAssembly/wasi-libc
+cd wasi-libc
+make
+cd ..
 
 # download older version of cmake for emscripten compatibility
 wget -qO- https://github.com/Kitware/CMake/releases/download/v3.23.3/cmake-3.23.3-linux-x86_64.tar.gz | tar -xz
 
 # build llvm
 git clone https://github.com/llvm/llvm-project
-echo 'set_target_properties(clang PROPERTIES LINK_FLAGS "--embed-file=sysroot --max-memory=4294967296")' >> llvm-project/llvm/CMakeLists.txt
+echo 'set_target_properties(clang PROPERTIES LINK_FLAGS --embed-file=sysroot)' >> llvm-project/llvm/CMakeLists.txt
 emcmake ./cmake-3.23.3-linux-x86_64/bin/cmake -G Ninja -S llvm-project/llvm -B web-llvm-build \
         -DCMAKE_BUILD_TYPE=MinSizeRel \
         -DLLVM_ENABLE_PROJECTS="clang" \
@@ -38,6 +37,5 @@ emcmake ./cmake-3.23.3-linux-x86_64/bin/cmake -G Ninja -S llvm-project/llvm -B w
         -DLLVM_CCACHE_DIR=/tmp/ccache \
         -DDEFAULT_SYSROOT=/sysroot \
         -DCMAKE_CXX_FLAGS='-Dwait4=__syscall_wait4 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE'
-# mv wasi-libc/sysroot web-llvm-build
-mv wasi-sysroot web-llvm-build/sysroot
+mv wasi-libc/sysroot web-llvm-build
 ninja -C web-llvm-build -- clang
