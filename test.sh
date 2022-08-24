afb9a67a5e5cd1f1e49acdcc1843586b5e462a2d
Alistair Keiller <alistair@keiller.net>
2022-08-24 15:47:30 -0700

    test

--- test.sh
+++ test.sh
@@ -26,7 +26,6 @@ wget -qO- https://github.com/Kitware/CMake/releases/download/v3.23.3/cmake-3.23.
 git clone https://github.com/llvm/llvm-project
 echo 'set_target_properties(clang PROPERTIES LINK_FLAGS --embed-file=sysroot)' >> llvm-project/llvm/CMakeLists.txt
 echo 'set_target_properties(clang PROPERTIES LINK_FLAGS --embed-file=lib/clang/16.0.0/include)' >> llvm-project/llvm/CMakeLists.txt
-echo 'set_target_properties(clang PROPERTIES LINK_FLAGS --initial-memory=33554432)' >> llvm-project/llvm/CMakeLists.txt
 emcmake ./cmake-3.23.3-linux-x86_64/bin/cmake -G Ninja -S llvm-project/llvm -B web-llvm-build \
         -DCMAKE_BUILD_TYPE=MinSizeRel \
         -DLLVM_ENABLE_PROJECTS="clang" \
@@ -37,6 +36,6 @@ emcmake ./cmake-3.23.3-linux-x86_64/bin/cmake -G Ninja -S llvm-project/llvm -B w
         -DLLVM_INCLUDE_TESTS=OFF \
         -DLLVM_CCACHE_BUILD=ON \
         -DLLVM_CCACHE_DIR=/tmp/ccache \
-        -DCMAKE_CXX_FLAGS='-Dwait4=__syscall_wait4 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE'
+        -DCMAKE_CXX_FLAGS='-Dwait4=__syscall_wait4 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE -sINITIAL_MEMORY=32MB'
 mv wasi-libc/sysroot web-llvm-build
 ninja -C web-llvm-build -- clang
