713fb18d94711db41e40c5a8f612323626fe0379
Alistair Keiller <alistair@keiller.net>
2022-08-28 13:52:03 -0700

    remove wasmfs

--- test.sh
+++ test.sh
@@ -20,7 +20,7 @@ wget -qO- https://github.com/Kitware/CMake/releases/download/v3.23.3/cmake-3.23.
 git clone https://github.com/llvm/llvm-project
 echo 'set_target_properties(clang PROPERTIES LINK_FLAGS --embed-file=lib/clang)' >> llvm-project/llvm/CMakeLists.txt
 CXXFLAGS="-Dwait4=__syscall_wait4" \
-LDFLAGS='-sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE -sINITIAL_MEMORY=32MB -sWASM_BIGINT -sWASMFS -sENVIRONMENT=web' \
+LDFLAGS='-sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE -sINITIAL_MEMORY=32MB -sWASM_BIGINT -sENVIRONMENT=web' \
 emcmake ./cmake-3.23.3-linux-x86_64/bin/cmake -G Ninja -S llvm-project/llvm -B web-llvm-build \
         -DCMAKE_BUILD_TYPE=Release \
         -DLLVM_ENABLE_PROJECTS="clang;lld" \
