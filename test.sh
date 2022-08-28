8f25e7a8f123fafe3175f534868dcf11c89f2e0d
Alistair Keiller <alistair@keiller.net>
2022-08-27 21:15:01 -0700

    test

--- test.sh
+++ test.sh
@@ -22,6 +22,7 @@ wget -qO- https://github.com/Kitware/CMake/releases/download/v3.23.3/cmake-3.23.
 # build llvm
 git clone https://github.com/llvm/llvm-project
 echo 'set_target_properties(clang PROPERTIES LINK_FLAGS --embed-file=lib/clang)' >> llvm-project/llvm/CMakeLists.txt
+# CXX_FLAGS='-Dwait4=__syscall_wait4' \
 LDFLAGS='-sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE -sINITIAL_MEMORY=32MB -sWASM_BIGINT -sWASMFS -sENVIRONMENT=web --closure 1' \
 emcmake ./cmake-3.23.3-linux-x86_64/bin/cmake -G Ninja -S llvm-project/llvm -B web-llvm-build \
         -DCMAKE_BUILD_TYPE=Release \
@@ -34,8 +35,7 @@ emcmake ./cmake-3.23.3-linux-x86_64/bin/cmake -G Ninja -S llvm-project/llvm -B w
         -DLLVM_INCLUDE_EXAMPLES=OFF \
         -DLLVM_INCLUDE_TESTS=OFF \
         -DLLVM_CCACHE_BUILD=ON \
-        -DLLVM_CCACHE_DIR=/tmp/ccache \
-        # -DCMAKE_CXX_FLAGS='-Dwait4=__syscall_wait4 -sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE -sINITIAL_MEMORY=32MB -sWASM_BIGINT -sWASMFS -sENVIRONMENT=web --closure 1'
+        -DLLVM_CCACHE_DIR=/tmp/ccache
 mkdir -p web-llvm-build/lib/clang
 mv wasi-sysroot web-llvm-build/lib/clang/wasi
 ninja -C web-llvm-build -- clang lld
