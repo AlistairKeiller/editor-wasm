6f6aaad2c246ded2bd985b17f746ff4fa15511d7
Alistair Keiller <alistair@keiller.net>
2022-08-31 11:46:46 -0700

    use emscripten tot

--- test.sh
+++ test.sh
@@ -5,17 +5,14 @@ set -e
 # install emscripten
 git clone https://github.com/emscripten-core/emsdk
 cd emsdk
-./emsdk install 3.1.19
-./emsdk activate 3.1.19
+./emsdk install tot
+./emsdk activate tot
 . ./emsdk_env.sh
 cd ..
 
 # download sysroot
 wget -qO- https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-16/wasi-sysroot-16.0.tar.gz | tar -xz
 wget -qO- https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-16/libclang_rt.builtins-wasm32-wasi-16.0.tar.gz | tar -xz
-
-# download older version of cmake for emscripten compatibility ( should remove in emscripten 3.1.21 )
-wget -qO- https://github.com/Kitware/CMake/releases/download/v3.23.3/cmake-3.23.3-linux-x86_64.tar.gz | tar -xz
 
 # build llvm
 git clone https://github.com/llvm/llvm-project
@@ -24,7 +21,7 @@ echo "set_target_properties(clang PROPERTIES LINK_FLAGS --embed-file=lib/clang/1
 set_target_properties(lld PROPERTIES LINK_FLAGS --embed-file=wlib)" >> llvm-project/llvm/CMakeLists.txt
 CXXFLAGS="-Dwait4=__syscall_wait4" \
 LDFLAGS='-sEXPORTED_RUNTIME_METHODS=FS,callMain -sALLOW_MEMORY_GROWTH -sEXPORT_ES6 -sMODULARIZE -sINITIAL_MEMORY=32MB -sWASM_BIGINT -sENVIRONMENT=web' \
-emcmake ./cmake-3.23.3-linux-x86_64/bin/cmake -G Ninja -S llvm-project/llvm -B web-llvm-build \
+emcmake cmake -G Ninja -S llvm-project/llvm -B web-llvm-build \
         -DCMAKE_BUILD_TYPE=Release \
         -DLLVM_ENABLE_PROJECTS="clang;lld" \
         -DLLVM_TARGETS_TO_BUILD=WebAssembly \
@@ -40,6 +37,6 @@ emcmake ./cmake-3.23.3-linux-x86_64/bin/cmake -G Ninja -S llvm-project/llvm -B w
 mkdir -p web-llvm-build/lib/clang/16.0.0
 cp -r wasi-sysroot/include web-llvm-build/lib/clang/16.0.0/include
 cp -r wasi-sysroot/lib/wasm32-wasi web-llvm-build/wlib
-cp -r lib/wasi/libclang_rt.builtins-wasm32.a web-llvm-build/wlib/libclang_rt.builtins-wasm32.a
+cp lib/wasi/libclang_rt.builtins-wasm32.a web-llvm-build/wlib/libclang_rt.builtins-wasm32.a
 
 ninja -C web-llvm-build -- clang lld
